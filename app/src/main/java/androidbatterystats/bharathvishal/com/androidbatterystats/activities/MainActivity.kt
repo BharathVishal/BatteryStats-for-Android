e4b21e03df106994cc1a15e5afe04ac0040cfe70
Bharath Vishal G <bharathvishalg@gmail.com>
2023-02-19 23:13:32 +0530

    Dev commit

--- app/src/main/java/androidbatterystats/bharathvishal/com/androidbatterystats/activities/MainActivity.kt
+++ app/src/main/java/androidbatterystats/bharathvishal/com/androidbatterystats/activities/MainActivity.kt
@@ -13,8 +13,25 @@ import androidbatterystats.bharathvishal.com.androidbatterystats.databinding.Act
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.color.DynamicColors
 
-
 /**
+ *
+ * Copyright 2018-2023 Bharath Vishal G.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **/
+
+/*
  * Created by Bharath Vishal on 20-01-2018.
  */
 
