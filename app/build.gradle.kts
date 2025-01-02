1daf867486979410657cbc057fcdecce9ed66f3e
Bharath Vishal G <bharathvishalg@gmail.com>
2025-01-02 20:21:16 +0530

    Dev commit
    - Upgraded gradle plugin to 8.9.0.
    - Updated all internal dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.bharathvishal.batterystatsforandroid"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.bharathvishal.batterystatsforandroid"
         vectorDrawables {
